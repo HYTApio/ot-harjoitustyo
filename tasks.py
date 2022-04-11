6103c77c8744b38041f1f0a58dc3cc4512f20d17
HYTapio <tapio.uzal@helsinki.fi>
2022-04-11 13:26:56 +0300

    added new tasks

--- tasks.py
+++ tasks.py
@@ -10,8 +10,13 @@ def test(ctx):
 
 @task
 def coverage(ctx):
-    ctx.run("coverage run --branch -m pytest", pty=True)
+    ctx.run("cd src/", pty=True)
+    ctx.run("coverage run --branch -m pytest src", pty=True)
 
 @task(coverage)
 def coverage_report(ctx):
     ctx.run("coverage html", pty=True)
+
+@task
+def lint(ctx):
+    ctx.run("pylint src", pty=True)
