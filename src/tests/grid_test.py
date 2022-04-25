eb9230e0220c536905b97686ea0fe6a92b377200
HYTapio <tapio.uzal@helsinki.fi>
2022-04-25 21:46:06 +0300

    added new tests

--- src/tests/grid_test.py
+++ src/tests/grid_test.py
@@ -5,8 +5,12 @@ from grid import *
 class TestGrid(unittest.TestCase):
     def setUp(self):
         self.window = (800, 700)
-        self.locked_positions = {(5, 18): (127, 255, 0), (6, 18): (127, 255, 0), (4, 19): (127, 255, 0), (5, 19): (127, 255, 0)}
-    
+        self.locked_positions = {(5, 18): (127, 255, 0), (6, 18): (127, 255, 0), (4, 19): (127, 255, 0), (5, 19): (127, 255, 0), (4, 18): (127, 255, 0), (3, 18): (127, 255, 0), (2, 18): (127, 255, 0), (1, 18): (127, 255, 0), (0, 18): (127, 255, 0), (7, 18): (127, 255, 0), (8, 18): (127, 255, 0), (9, 18): (127, 255, 0),}
+        self.grid = create_grid(self.locked_positions)
+
     def test_draw_grid_works(self):
         grid = create_grid(self.locked_positions)
         self.assertEqual(grid[18][5], (127, 255, 0))
+
+    def test_clear_rows_work(self):
+        self.assertEqual(clear_rows(self.grid,self.locked_positions), 1)
--- src/tests/pieces_test.py
+++ src/tests/pieces_test.py
@@ -1,11 +1,14 @@
 import unittest
 from pieces import *
 from shapes import *
+from grid import *
 
 
 class TestPieces(unittest.TestCase):
     def setUp(self):
         self.piece = Pieces(5, 0, 1)
+        self.locked = {}
+        self.grid = create_grid(self.locked)
 
     def test_get_shape_works(self):
         test_shape = get_shape()
@@ -18,3 +21,27 @@ class TestPieces(unittest.TestCase):
         self.assertEqual(test_shape.y_row, 0)
         self.assertEqual(test_shape.color, piece_colors(shapenumber))
         self.assertEqual(test_shape.shape, piece_shape(shapenumber))
+
+    def test_piece_positions_works(self):
+        self.piece.x_row-=1
+        self.assertEqual(piece_positions(self.piece), [(3, -2), (4, -2), (3, -1), (4, -1)])
+    
+    def test_free_space(self):
+        self.assertEqual(free_space(self.piece, self.grid), True)
+        self.piece.y_row+=5
+        piece_position = piece_positions(self.piece)
+        for square in piece_position:
+            position = (square[0], square[1])
+            self.locked[position] = self.piece.color
+        self.grid = create_grid(self.locked)
+        self.assertEqual(free_space(self.piece, self.grid), False)
+    
+    def test_game_lost_works(self):
+        self.assertEqual(game_lost(self.locked), False)
+        piece_position = piece_positions(self.piece)
+        for square in piece_position:
+            position = (square[0], square[1])
+            self.locked[position] = self.piece.color
+        self.assertEqual(game_lost(self.locked), True)
+
+        
