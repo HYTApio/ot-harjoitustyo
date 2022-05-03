10aec9a235a935ac026e613c7a5af14f58ee03c0
HYTapio <tapio.uzal@helsinki.fi>
2022-05-03 11:28:54 +0300

    added main_menu to be first to run

--- src/index.py
+++ src/index.py
@@ -1,9 +1,10 @@
 import pygame
-from gameloop import GameLoop
+from ui.main_menu import main_menu
 
 def main():
-    game = GameLoop()
     pygame.display.set_caption("Tetris")
-    game.start()
+    window = pygame.display.set_mode((1000, 800))
+    main_menu(window)
+
 if __name__ == "__main__":
     main()
